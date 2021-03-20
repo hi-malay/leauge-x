173f37fd10f508a35956514fe247c9e4f7684ace
malay3 <malay@goodhealthapp.com>
2021-03-20 12:43:20 +0530

    twaek

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -14,7 +14,7 @@ class CustomRoute extends Component {
     render() {
         return (
             <div>
-                <Router basename="/leauge-x">
+                <Router basename="/league-x">
                     <Switch>
                         <PrivateRoute path="/home" component={HomePage} />
                         <PrivateRoute path="/game" component={GamePage} />
